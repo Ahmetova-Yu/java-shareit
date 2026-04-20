c032501bb073673ae9728deff785c0b04d5b3850
UAhmetova <uliaahm.com@yandex.ru>
2026-04-20 23:14:53 +0300

    Добавление глобального обработчика ошибок

---
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -0,0 +1,28 @@
+package ru.practicum.shareit.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNoSuchElementException(NoSuchElementException e) {
+        return Map.of("Ошибка", e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleRuntimeException(RuntimeException e) {
+        if (e.getMessage().contains("not found") || e.getMessage().contains("Не найден")) {
+            return Map.of("Ошибка", e.getMessage());
+        }
+        return Map.of("Ошибка", e.getMessage());
+    }
+}
