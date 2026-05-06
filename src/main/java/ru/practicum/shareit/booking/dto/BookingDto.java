b3159823708015dbe40b0fef32362c850f4db2a9
UAhmetova <uliaahm.com@yandex.ru>
2026-05-06 18:30:36 +0300

    Добавление dto классов

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,13 @@
 package ru.practicum.shareit.booking.dto;
 
-/**
- * TODO Sprint add-bookings.
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookingDto {
+    private Long id;
+    private Long bookerId;
 }
