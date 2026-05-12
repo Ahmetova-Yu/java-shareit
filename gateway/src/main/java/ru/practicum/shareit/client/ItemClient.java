be3b3aa104a3c5d7fda88060154a5d93218cfdc4
UAhmetova <uliaahm.com@yandex.ru>
2026-05-12 17:40:04 +0300

    правки

--- gateway/src/main/java/ru/practicum/shareit/client/ItemClient.java
+++ gateway/src/main/java/ru/practicum/shareit/client/ItemClient.java
@@ -5,13 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.shareit.item.dto.CommentRequestDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import java.util.Map;
 
-@Service
+@Component
 public class ItemClient extends BaseClient {
     private static final String API_PREFIX = "/items";
 
