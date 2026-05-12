878ac6285db195b21f64935bd52452f7ee8d9948
UAhmetova <uliaahm.com@yandex.ru>
2026-05-12 16:18:25 +0300

    Добавление сущности booking

---
+++ server/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -0,0 +1,49 @@
+package ru.practicum.shareit.booking;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingRequestDto;
+import ru.practicum.shareit.booking.model.BookingState;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bookings")
+@RequiredArgsConstructor
+public class BookingController {
+    private final BookingService bookingService;
+
+    @PostMapping
+    public BookingDto create(@RequestHeader("X-Sharer-User-Id") Long userId,
+                             @RequestBody BookingRequestDto requestDto) {
+        return bookingService.create(userId, requestDto);
+    }
+
+    @PatchMapping("/{bookingId}")
+    public BookingDto approve(@RequestHeader("X-Sharer-User-Id") Long userId,
+                              @PathVariable Long bookingId,
+                              @RequestParam Boolean approved) {
+        return bookingService.approve(userId, bookingId, approved);
+    }
+
+    @GetMapping("/{bookingId}")
+    public BookingDto getById(@RequestHeader("X-Sharer-User-Id") Long userId,
+                              @PathVariable Long bookingId) {
+        return bookingService.getById(userId, bookingId);
+    }
+
+    @GetMapping
+    public List<BookingDto> getAllByBooker(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                           @RequestParam(defaultValue = "ALL") String state) {
+        BookingState bookingState = BookingState.valueOf(state.toUpperCase());
+        return bookingService.getAllByBooker(userId, bookingState);
+    }
+
+    @GetMapping("/owner")
+    public List<BookingDto> getAllByOwner(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                          @RequestParam(defaultValue = "ALL") String state) {
+        BookingState bookingState = BookingState.valueOf(state.toUpperCase());
+        return bookingService.getAllByOwner(userId, bookingState);
+    }
+}
