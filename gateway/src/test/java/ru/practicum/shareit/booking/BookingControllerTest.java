8989fd464f9a518eaf2032d6cdfdc6a660bb7097
UAhmetova <uliaahm.com@yandex.ru>
2026-05-13 15:34:07 +0300

    Правки

--- gateway/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
+++ gateway/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
@@ -5,13 +5,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.booking.dto.BookItemRequestDto;
 import ru.practicum.shareit.client.BookingClient;
 
 import java.time.LocalDateTime;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,6 +36,9 @@ class BookingControllerTest {
     void shouldCreateBooking() throws Exception {
         BookItemRequestDto requestDto = new BookItemRequestDto(1L,
                 LocalDateTime.now().plusDays(1), LocalDateTime.now().plusDays(2));
+
+        when(bookingClient.bookItem(eq(2L), any(BookItemRequestDto.class)))
+                .thenReturn(ResponseEntity.status(HttpStatus.CREATED).body("{\"id\":1}"));
 
         mockMvc.perform(post("/bookings")
                         .header("X-Sharer-User-Id", 2L)
